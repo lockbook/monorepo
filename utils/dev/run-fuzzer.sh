acd76fa7c2249d2942faa54c297d9a9a1873ec39
raayan <raayan@raayanpillai.com>
2023-04-22 17:55:46 -0400

    release-lto

--- utils/dev/run-fuzzer.sh
+++ utils/dev/run-fuzzer.sh
@@ -9,4 +9,4 @@ projRoot=$(git rev-parse --show-toplevel)
 cd "$projRoot"/libs/core
 . ../../containers/local.env
 
-cargo test --test exhaustive_sync_check --release --features 'no-network' -- --nocapture --ignored
+cargo test --test exhaustive_sync_check --profile=release-lto --features 'no-network' -- --nocapture --ignored
