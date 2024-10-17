8678d69ab32b38a05d994d35ca6cea6a8e7d005d
“Travis <t.vanderstad@gmail.com>
2024-10-17 15:21:48 -0400

    add icons

--- libs/content/workspace/src/theme/icons.rs
+++ libs/content/workspace/src/theme/icons.rs
@@ -16,6 +16,8 @@ const fn ic(c: &'static str) -> Icon {
 impl Icon {
     pub const ACCOUNT: Self = ic("\u{e7ff}"); // Person Outline
     pub const ARROW_CIRCLE_DOWN: Self = ic("\u{f181}"); // Arrow Circle Down
+    pub const ARROW_UPWARD: Self = ic("\u{e5d8}"); // Arrow Upward
+    pub const ARROW_DOWNWARD: Self = ic("\u{e5db}"); // Arrow Downward
     pub const BRING_BACK: Self = ic("\u{e5cb}");
     pub const BRING_TO_BACK: Self = ic("\u{e5dc}");
     pub const BRING_TO_FRONT: Self = ic("\u{e5dd}");
